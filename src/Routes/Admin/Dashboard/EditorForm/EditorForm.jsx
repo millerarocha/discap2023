9fc827ad9b4a66039b2624d3ab4714fdc9cfd7aa
unilimcol <unilim.co@gmail.com>
2023-03-28 13:54:46 -0500

    feat/add ckeditor to input

--- src/Routes/Admin/Dashboard/EditorForm/EditorForm.jsx
+++ src/Routes/Admin/Dashboard/EditorForm/EditorForm.jsx
@@ -1,11 +1,15 @@
 import Input from "../../../../common/Input";
 
+
+
 const EditorForm = ({ title = "Diligencie los campos" }) => {
   return (
-    <div className="w-2/3 mx-auto flex flex-col bg-slate-300 p-6 rounded-lg gap-5">
+    <div className="w-2/3 mx-auto flex flex-col bg-slate-300 p-6 rounded-lg">
       <h2 className="font-bold text-lg uppercase text-center mb-5">{title}</h2>
-      <form>
+      <form className="flex flex-col gap-4">
         <Input label="Titulo de la noticia"/>
+        <Input label="Descripción de la noticia" isEditor/>
+        
       </form>
     </div>
   );
--- src/common/Input/Input.jsx
+++ src/common/Input/Input.jsx
@@ -1,16 +1,55 @@
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
+const editorConfiguration = {
+  toolbar: [
+    'undo',
+    'redo',
+    'heading',
+    '|',
+    'bold',
+    'italic',
+    'bulletedList',
+    'numberedList',
+    '|',
+    'link',
+  ],
+};
 
-const Input = ({label='input title'}) => {
+const Input = ({ label = "input title", placeholder, isEditor = false }) => {
   return (
     <div className="form-control w-full">
       <label className="label">
         <span className="label-text uppercase">{label}</span>
       </label>
-      <input
-        type="text"
-        placeholder="Type here"
-        className="input input-bordered w-full"
-      />
+      {isEditor ? (
+        <CKEditor
+          className="form-control w-full"
+          editor={ClassicEditor}
+          config={editorConfiguration}
+          data={placeholder}
+          onReady={(editor) => {
+            // You can store the "editor" and use when it is needed.
+            console.log("Editor is ready to use!", editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log("Blur.", editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log("Focus.", editor);
+          }}
+        />
+      ) : (
+        <input
+          type="text"
+          placeholder={placeholder}
+          className="input input-bordered w-full"
+        />
+      )}
     </div>
   );
 };
