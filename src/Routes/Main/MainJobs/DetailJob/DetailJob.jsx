b985a0065d7d9dd70e77426d9fedcb4ff9205ea9
unilimcol <unilim.co@gmail.com>
2023-06-01 14:04:29 -0500

    feat/add details section complete

--- src/Routes/Main/MainJobs/DetailJob/DetailJob.jsx
+++ src/Routes/Main/MainJobs/DetailJob/DetailJob.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SectionNav from "../../../../common/SectionNav/SectionNav";
+import { db } from "../../../../services/firebase.config";
+import { getDoc, doc } from "firebase/firestore";
+import { useParams } from "react-router-dom";
+import Details from "../../../../common/Details/Details";
 
 const DetailJob = () => {
+  const { id } = useParams();
+  const docRef = doc(db, "jobs", id);
+
+  const [data, setData] = useState(undefined);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const docSnap = await getDoc(docRef);
+        setData(docSnap.data());
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
     <>
-      <SectionNav jobsSection/>
-      DetailJob
+      <SectionNav jobsSection />
+      <Details data={data}/>
     </>
   );
 };
--- src/Routes/Main/MainNews/DetailNew/DetailNew.jsx
+++ src/Routes/Main/MainNews/DetailNew/DetailNew.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SectionNav from "../../../../common/SectionNav/SectionNav";
+import { db } from "../../../../services/firebase.config";
+import { getDoc, doc } from "firebase/firestore";
+import { useParams } from "react-router-dom";
+import Details from "../../../../common/Details/Details";
 
 const DetailNew = () => {
+  const { id } = useParams();
+  const docRef = doc(db, "news", id);
+
+  const [data, setData] = useState(undefined)
+
+  useEffect(() => {
+    const getData = async ()=>{
+      try {
+        const docSnap = await getDoc(docRef)
+        setData(docSnap.data())
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getData();
+  }, [])
+
   return (
     <>
       <SectionNav />
-      DetailNew
+      <Details data={data}/>
     </>
   );
 };
